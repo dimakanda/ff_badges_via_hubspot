0ecceb06b4d60c7d94e59a7f28de640a1c6fb989
Artur Szwed <artur1979@googlemail.com>
2013-10-08 17:53:05 +0200

    Add model callback to add event when user get new badge.

--- app/models/user_badge.rb
+++ app/models/user_badge.rb
@@ -7,11 +7,18 @@ class UserBadge < ActiveRecord::Base
 
   after_create lambda { |record| FfBadgesMailer.badge_earned_email(record.user, record.badge).deliver }
   after_create :set_badge_in_redis
+  after_create :add_event_badge_earn
 
   private
 
   def set_badge_in_redis
   	$redis.sadd "ff_badges_#{self.user_id}", self.badge.filename
+  end
+
+  def add_event_badge_earn
+  	if defined?(Event) == 'constant' && Event.superclass == ActiveRecord::Base #only if model Event is defined
+  		Event.add self.badge, 'badge_earn', self.user
+  	end
   end
 
 end
