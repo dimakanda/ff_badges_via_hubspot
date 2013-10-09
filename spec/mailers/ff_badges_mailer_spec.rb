a04352d8e978eb2a175b89dc7449d784a4367e52
Artur Szwed <artur1979@googlemail.com>
2013-10-09 16:56:35 +0200

    Spec ff_badges_mailer.

---
+++ spec/mailers/ff_badges_mailer_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+
+describe FfBadgesMailer do
+
+  describe 'badge_earned_email' do
+
+    it 'should contain correct contents' do
+      user = create :user
+      badge = create :badge
+
+      mail = FfBadgesMailer.badge_earned_email(user, badge).deliver
+
+      expect(mail.html_part.body.decoded).to have_content "you have earned #{badge.name} badge"
+      expect(mail.html_part.body.decoded).to have_content badge.message
+      expect(mail.subject).to eql "You have earned #{badge.name} badge."
+      expect(mail.to).to eql [user.email]
+    end
+  end
+
+end
