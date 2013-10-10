478a599d3d08b83dd1a0eb5fd5b83a3c3d323116
Artur Szwed <artur1979@googlemail.com>
2013-10-10 09:55:24 +0200

    Add module for showing badges included in app controllers.

---
+++ app/controllers/concerns/ff_badges/show_earned_badges.rb
@@ -0,0 +1,20 @@
+module Concerns::FfBadges::ShowEarnedBadges
+  extend ActiveSupport::Concern
+
+  included do
+    append_before_filter :show_earned_badges
+  end
+
+
+  private
+
+  def show_earned_badges
+    if logged_in? && request.get? && !request.xhr? && $redis && $redis.exists("ff_badges_#{current_user.id}")
+      if badge_filename = $redis.spop("ff_badges_#{current_user.id}")
+        @ff_badge = Badge.where(filename: badge_filename).first
+        gon.ff_badges_show_notification = true
+      end
+    end
+  end
+
+end
