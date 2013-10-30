4d04204f68331a9b97d41e0ebd11af4b060bd499
Artur Szwed <artur1979@googlemail.com>
2013-10-30 15:28:41 +0100

    Move badges observers definitions to extras/ff_badges.

--- lib/ff_badges/engine.rb
+++ lib/ff_badges/engine.rb
@@ -11,33 +11,35 @@ module FfBadges
       end
     end
 
-    initializer :add_callbacks do
+    initializer :config_redis do
+      $redis = Redis.new(host: 'localhost', port: 6379)
+    end
+
+    # adds a methods like deservers_xxxxx_badge? to User model
+    initializer :add_methods_for_badge_conditions do
       User.activated_badges.each do |badge_name|
         module_name = "FfBadges::Badges"
 
         User.send :include, "#{module_name}::#{badge_name.to_s.camelcase}".constantize
-        #to_include = "#{module_name}::UserConditions".split('::').inject(Object) {|o,c| o.const_get c}
-        #User.send :include, to_include
-
-        #require Rails.root + "app/observers/ff_badges/#{badge_name}_observer.rb"
-
       end
     end
 
-    initializer :add_gem_badges_observers do |app|
-      Dir.glob(root.to_s + "/lib/ff_badges/observers/*_observer.rb").each do |c|
-        file = File.basename(c)
+    initializer :add_badges_observers do |app|
+
+      # observers defined in gem
+      observers = Dir.glob(root.to_s + "/lib/ff_badges/observers/*_observer.rb")
+      # observers defined in app
+      observers += Dir.glob(app.root.to_s + "/extras/ff_badges/observers/*_observer.rb")
+
+      Dir.glob(observers).each do |path|
+        file = File.basename(path)
         badge_filename = file.split('_observer.rb').first
 
         if User.badge_activated? badge_filename
-          require_dependency(c)
+          require_dependency(path)
           ActiveRecord::Base.observers += ["FfBadges::Observers::#{badge_filename.camelcase}Observer"]
         end
       end
-    end
-
-    initializer :config_redis do
-      $redis = Redis.new(host: 'localhost', port: 6379)
     end
 
     config.generators do |g|
