a39ad22db487c6ed7b3dfe30c356201205e956c8
Artur Szwed <artur1979@googlemail.com>
2013-11-28 14:47:56 +0100

    Add more validations and comments in form for badges.

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -5,14 +5,15 @@ class Badge < ActiveRecord::Base
 
 	has_attached_file :icon, styles: { big: "200x200#", medium: "73x73#", thumb: "48x48#" }
 
-	validates_presence_of :name, :message, :icon, :points, :filename
-	validates :points, numericality: { only_integer: true, greater_than: 0 }
+	validates_presence_of :name, :message, :icon, :filename
+	validates :points, numericality: { only_integer: true, greater_than: 0 }, allow_blank: true
 	validates_attachment :icon,
     content_type: { content_type: ['image/gif', 'image/png', 'image/x-png', 'image/jpeg', 'image/pjpeg', 'image/jpg']},
     size: { in: 0..5.megabytes }
   validates :invertable, inclusion: {in: [true, false]}
 
   validates_uniqueness_of :filename, :name
+  validates :filename, format: { with: /\A[a-z0-9_]+\z/, message: "has wrong format" }
 
   attr_accessible :name, :description, :message, :filename, :points, :icon, :invertable
 
--- spec/models/badge_spec.rb
+++ spec/models/badge_spec.rb
@@ -8,13 +8,12 @@ describe Badge do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:message) }
   it { should validate_presence_of(:icon) }
-  it { should validate_presence_of(:points) }
   it { should validate_presence_of(:filename) }
   it { should have_attached_file(:icon) }
   it { should validate_numericality_of(:points).only_integer }
 
   it 'should validate uniqueness of name' do
-    badge = create :badge, name: 'Foo'
+    create :badge, name: 'Foo'
     badge2 = build :badge, name: 'Foo'
 
     expect(badge2).not_to be_valid
@@ -22,11 +21,22 @@ describe Badge do
   end
 
   it 'should validate uniqueness of filename' do
-    badge = create :badge, filename: 'foo'
+    create :badge, filename: 'foo'
     badge2 = build :badge, filename: 'foo'
 
     expect(badge2).not_to be_valid
     expect(badge2.errors[:filename]).to match_array ['has already been taken']
+  end
+
+  it 'should validate format of filename' do
+    badge1 = build :badge, filename: 'my_badge'
+    badge2 = build :badge, filename: 'foobadge1'
+    badge3 = build :badge, filename: 'foo-badge'
+
+    expect(badge1).to be_valid
+    expect(badge2).to be_valid
+    expect(badge3).not_to be_valid
+    expect(badge3.errors[:filename]).to match_array ['has wrong format']
   end
 
 end
