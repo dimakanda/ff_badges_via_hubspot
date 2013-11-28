d8ec45fb0e9c8da850c496ce01f8557b2803a7d5
Artur Szwed <artur1979@googlemail.com>
2013-11-28 14:14:26 +0100

    Remove running migrations from engine.

--- lib/ff_badges/engine.rb
+++ lib/ff_badges/engine.rb
@@ -1,16 +1,6 @@
 module FfBadges
   class Engine < ::Rails::Engine
 
-    # it allows to run migrations without copying it to the main db/migrate folder
-    # http://pivotallabs.com/leave-your-migrations-in-your-rails-engines/
-    initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
-        config.paths["db/migrate"].expanded.each do |expanded_path|
-          app.config.paths["db/migrate"] << expanded_path
-        end
-      end
-    end
-
     initializer :config_redis do
       $redis = Redis.new(host: 'localhost', port: 6379)
     end
