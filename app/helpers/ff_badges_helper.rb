ef3f4811af32b2fa01693476a175303e74497257
Artur Szwed <artur1979@googlemail.com>
2014-01-21 21:04:44 +0100

    Cache modal path in class variable.

--- app/helpers/ff_badges_helper.rb
+++ app/helpers/ff_badges_helper.rb
@@ -2,11 +2,12 @@ module FfBadgesHelper
 
 	def notice_earned_badges
 		if @ff_badge
-			app_partial_path = 'extras/ff_badges/views/modal.html.erb' 
+			app_badges_modal_path = 'extras/ff_badges/views/modal' 
 			
 			# if modal defined in app_partial_path than use it, otherwise use partial form engine
-			if File.exists?(app_partial_path) 
-				render file: app_partial_path, locals: { badge: @ff_badge }
+			if defined? @@app_badges_modal_path || File.exists?("#{app_badges_modal_path}.html.erb")
+				@@app_badges_modal_path ||= app_badges_modal_path # cache path in class variable
+				render file: @@app_badges_modal_path, locals: { badge: @ff_badge }
 			else
 				render template: 'shared/badges_modal', locals: { badge: @ff_badge }
 			end
--- lib/ff_badges/version.rb
+++ lib/ff_badges/version.rb
@@ -1,3 +1,3 @@
 module FfBadges
-  VERSION = "1.3.2"
+  VERSION = "1.3.3"
 end
