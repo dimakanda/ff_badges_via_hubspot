5006de786f64a811b02dc9bba34eae65e831e118
Artur Szwed <artur1979@gmail.com>
2014-03-04 18:43:07 +0100

    Set path and url for badge icon paperclip attachment.

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -3,7 +3,9 @@ class Badge < ActiveRecord::Base
   has_many :user_badges, dependent: :destroy
   has_many :users, through: :user_badges
 
-	has_attached_file :icon, styles: { big: "200x200#", medium: "73x73#", thumb: "48x48#" }
+	has_attached_file :icon, styles: { big: "200x200#", medium: "73x73#", thumb: "48x48#" },
+    path: ":rails_root/public/system/badges/icons/:id_partition/:style/:filename",
+    url: "/system/badges/icons/:id_partition/:style/:filename"
 
 	validates_presence_of :name, :message, :icon, :filename
 	validates :points, numericality: { only_integer: true, greater_than: 0 }, allow_blank: true
