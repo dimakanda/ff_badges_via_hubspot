da61f03c79e1784acfc022777aab8a8cecaef5aa
Kamil Sarna <kamil.sarna@gmail.com>
2014-12-01 19:08:09 +0100

    Enlarge the smallest image of a badge

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -3,11 +3,11 @@ class Badge < ActiveRecord::Base
   has_many :user_badges, dependent: :destroy
   has_many :users, through: :user_badges
 
-  has_attached_file :icon, styles: { big: "140x90#", medium: "112x72#", thumb: "48x31#" },
+  has_attached_file :icon, styles: { big: "140x90#", medium: "112x72#", thumb: "64x41#" },
                     path: ":rails_root/public/system/badges/active/:id_partition/:style/:filename",
                     url: "/system/badges/active/:id_partition/:style/:filename"
 
-  has_attached_file :icon_inactive, styles: { big: "140x90#", medium: "112x72#", thumb: "48x31#" },
+  has_attached_file :icon_inactive, styles: { big: "140x90#", medium: "112x72#", thumb: "64x41#" },
                     path: ":rails_root/public/system/badges/inactive/:id_partition/:style/:filename",
                     url: "/system/badges/inactive/:id_partition/:style/:filename"
 
