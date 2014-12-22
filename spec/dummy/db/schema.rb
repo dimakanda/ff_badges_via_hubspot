bc246988183d841c53f72fc8199d2708566f5d64
Kamil Sarna <kamil.sarna@gmail.com>
2014-12-22 18:09:19 +0100

    Add earned/unearned description to badges

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -9,11 +9,11 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131030162157) do
+ActiveRecord::Schema.define(version: 20141222162021) do
 
-  create_table "activities", :force => true do |t|
+  create_table "activities", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
@@ -22,23 +22,24 @@ ActiveRecord::Schema.define(:version => 20131030162157) do
     t.string   "action"
   end
 
-  add_index "activities", ["trackable_type"], :name => "index_activities_on_trackable_type"
-  add_index "activities", ["user_id", "trackable_id"], :name => "index_activities_on_user_id_and_trackable_id"
+  add_index "activities", ["trackable_type"], name: "index_activities_on_trackable_type", using: :btree
+  add_index "activities", ["user_id", "trackable_id"], name: "index_activities_on_user_id_and_trackable_id", using: :btree
 
-  create_table "administrators", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "administrators", force: true do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "badges", :force => true do |t|
-    t.string   "name",                                    :null => false
-    t.text     "external_description"
+  create_table "badges", force: true do |t|
+    t.string   "name",                                          null: false
+    t.text     "external_earned_description"
+    t.text     "external_unearned_description"
     t.text     "internal_description"
-    t.boolean  "secret",               :default => false
+    t.boolean  "secret",                        default: false
     t.text     "message"
-    t.string   "filename",                                :null => false
+    t.string   "filename",                                      null: false
     t.integer  "points"
-    t.boolean  "invertable",           :default => false
+    t.boolean  "invertable",                    default: false
     t.string   "icon_file_name"
     t.string   "icon_content_type"
     t.integer  "icon_file_size"
@@ -47,11 +48,11 @@ ActiveRecord::Schema.define(:version => 20131030162157) do
     t.string   "icon_inactive_content_type"
     t.integer  "icon_inactive_file_size"
     t.datetime "icon_inactive_updated_at"
-    t.datetime "created_at",                              :null => false
-    t.datetime "updated_at",                              :null => false
+    t.datetime "created_at",                                    null: false
+    t.datetime "updated_at",                                    null: false
   end
 
-  create_table "blog_posts", :force => true do |t|
+  create_table "blog_posts", force: true do |t|
     t.integer  "user_id"
     t.string   "title"
     t.text     "text"
@@ -60,76 +61,76 @@ ActiveRecord::Schema.define(:version => 20131030162157) do
     t.datetime "updated_at"
   end
 
-  add_index "blog_posts", ["user_id"], :name => "index_blog_posts_on_user_id"
+  add_index "blog_posts", ["user_id"], name: "index_blog_posts_on_user_id", using: :btree
 
-  create_table "oauth_access_grants", :force => true do |t|
-    t.integer  "resource_owner_id", :null => false
-    t.integer  "application_id",    :null => false
-    t.string   "token",             :null => false
-    t.integer  "expires_in",        :null => false
-    t.string   "redirect_uri",      :null => false
-    t.datetime "created_at",        :null => false
+  create_table "oauth_access_grants", force: true do |t|
+    t.integer  "resource_owner_id", null: false
+    t.integer  "application_id",    null: false
+    t.string   "token",             null: false
+    t.integer  "expires_in",        null: false
+    t.string   "redirect_uri",      null: false
+    t.datetime "created_at",        null: false
     t.datetime "revoked_at"
     t.string   "scopes"
   end
 
-  add_index "oauth_access_grants", ["token"], :name => "index_oauth_access_grants_on_token", :unique => true
+  add_index "oauth_access_grants", ["token"], name: "index_oauth_access_grants_on_token", unique: true, using: :btree
 
-  create_table "oauth_access_tokens", :force => true do |t|
+  create_table "oauth_access_tokens", force: true do |t|
     t.integer  "resource_owner_id"
-    t.integer  "application_id",    :null => false
-    t.string   "token",             :null => false
+    t.integer  "application_id",    null: false
+    t.string   "token",             null: false
     t.string   "refresh_token"
     t.integer  "expires_in"
     t.datetime "revoked_at"
-    t.datetime "created_at",        :null => false
+    t.datetime "created_at",        null: false
     t.string   "scopes"
   end
 
-  add_index "oauth_access_tokens", ["refresh_token"], :name => "index_oauth_access_tokens_on_refresh_token", :unique => true
-  add_index "oauth_access_tokens", ["resource_owner_id"], :name => "index_oauth_access_tokens_on_resource_owner_id"
-  add_index "oauth_access_tokens", ["token"], :name => "index_oauth_access_tokens_on_token", :unique => true
+  add_index "oauth_access_tokens", ["refresh_token"], name: "index_oauth_access_tokens_on_refresh_token", unique: true, using: :btree
+  add_index "oauth_access_tokens", ["resource_owner_id"], name: "index_oauth_access_tokens_on_resource_owner_id", using: :btree
+  add_index "oauth_access_tokens", ["token"], name: "index_oauth_access_tokens_on_token", unique: true, using: :btree
 
-  create_table "oauth_applications", :force => true do |t|
-    t.string   "name",         :null => false
-    t.string   "uid",          :null => false
-    t.string   "secret",       :null => false
-    t.string   "redirect_uri", :null => false
+  create_table "oauth_applications", force: true do |t|
+    t.string   "name",         null: false
+    t.string   "uid",          null: false
+    t.string   "secret",       null: false
+    t.string   "redirect_uri", null: false
     t.integer  "owner_id"
     t.string   "owner_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "oauth_applications", ["owner_id", "owner_type"], :name => "index_oauth_applications_on_owner_id_and_owner_type"
-  add_index "oauth_applications", ["uid"], :name => "index_oauth_applications_on_uid", :unique => true
+  add_index "oauth_applications", ["owner_id", "owner_type"], name: "index_oauth_applications_on_owner_id_and_owner_type", using: :btree
+  add_index "oauth_applications", ["uid"], name: "index_oauth_applications_on_uid", unique: true, using: :btree
 
-  create_table "sessions", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "sessions", force: true do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "subscribers", :force => true do |t|
+  create_table "subscribers", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email"
   end
 
-  create_table "user_badges", :force => true do |t|
-    t.integer  "user_id",        :null => false
-    t.integer  "badge_id",       :null => false
+  create_table "user_badges", force: true do |t|
+    t.integer  "user_id",        null: false
+    t.integer  "badge_id",       null: false
     t.string   "badge_filename"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
-  add_index "user_badges", ["user_id", "badge_filename"], :name => "index_user_badges_on_user_id_and_badge_filename", :unique => true
-  add_index "user_badges", ["user_id", "badge_id"], :name => "index_user_badges_on_user_id_and_badge_id", :unique => true
-  add_index "user_badges", ["user_id"], :name => "index_user_badges_on_user_id"
+  add_index "user_badges", ["user_id", "badge_filename"], name: "index_user_badges_on_user_id_and_badge_filename", unique: true, using: :btree
+  add_index "user_badges", ["user_id", "badge_id"], name: "index_user_badges_on_user_id_and_badge_id", unique: true, using: :btree
+  add_index "user_badges", ["user_id"], name: "index_user_badges_on_user_id", using: :btree
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: true do |t|
     t.string   "name"
-    t.string   "email",                                              :null => false
+    t.string   "email",                                           null: false
     t.string   "crypted_password"
     t.string   "salt"
     t.string   "activation_state"
@@ -140,8 +141,8 @@ ActiveRecord::Schema.define(:version => 20131030162157) do
     t.datetime "reset_password_email_sent_at"
     t.string   "remember_me_token"
     t.datetime "remember_me_token_expires_at"
-    t.datetime "created_at",                                         :null => false
-    t.datetime "updated_at",                                         :null => false
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
@@ -149,12 +150,12 @@ ActiveRecord::Schema.define(:version => 20131030162157) do
     t.string   "new_email"
     t.string   "new_email_token"
     t.datetime "new_email_token_expires_at"
-    t.boolean  "admin",                           :default => false
+    t.boolean  "admin",                           default: false
   end
 
-  add_index "users", ["activation_token"], :name => "index_users_on_activation_token"
-  add_index "users", ["new_email_token"], :name => "index_users_on_new_email_token"
-  add_index "users", ["remember_me_token"], :name => "index_users_on_remember_me_token"
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token"
+  add_index "users", ["activation_token"], name: "index_users_on_activation_token", using: :btree
+  add_index "users", ["new_email_token"], name: "index_users_on_new_email_token", using: :btree
+  add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", using: :btree
 
 end
