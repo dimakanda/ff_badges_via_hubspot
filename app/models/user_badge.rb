91026b67417816a151040090b9a35e459f5bcf6c
Eloy Gomez <eloy@indeos.es>
2016-06-20 17:43:04 +0200

    Add user defined callbacks

--- app/models/user_badge.rb
+++ app/models/user_badge.rb
@@ -8,11 +8,32 @@ class UserBadge < ActiveRecord::Base
   validates :user_id, :badge_id, presence: true
   validates_uniqueness_of :badge_id, scope: :user_id
 
-  after_create :send_badge_email
-  after_create :set_badge_in_redis
-  after_create :add_event_badge_earn
+  after_create :after_create_actions
+
+
+  # User callbacks
+  #----------------------------------------------------------------------
+  @@user_callbacks = {}
+
+  def self.on_creation(&block)
+    @@user_callbacks[:after_create] ||= []
+    @@user_callbacks[:after_create].push block
+  end
 
   private
+
+  def after_create_actions
+    send_badge_email
+    set_badge_in_redis
+    add_event_badge_earn
+
+    # Run user callbacks
+    if @@user_callbacks[:after_create].present?
+      @@user_callbacks[:after_create].each do |callback|
+        callback.call(self)
+      end
+    end
+  end
 
   def send_badge_email
     FfBadgesMailer.badge_earned_email(user, badge).deliver_now unless skip_email
@@ -22,6 +43,7 @@ class UserBadge < ActiveRecord::Base
     $redis.sadd "ff_badges_#{self.user_id}", self.badge.filename
   end
 
+  # TODO: Move this callback back to the project where it belongs
   def add_event_badge_earn
     if defined?(Event) == 'constant' && Event.superclass == ActiveRecord::Base #only if model Event is defined
       Event.add self.badge, 'badge_earn', self.user
