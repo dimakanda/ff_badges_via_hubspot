e257e3d437fc437849ca0de819f0ff8636e0cb2e
Robert Beene <robert@echobind.com>
2018-09-26 12:14:48 -0400

    Make docker the default URL

--- lib/ff_badges/engine.rb
+++ lib/ff_badges/engine.rb
@@ -5,7 +5,7 @@ module FfBadges
       if ENV['REDIS_URL']
         $redis = Redis.new(url: ENV['REDIS_URL'])
       else
-        $redis = Redis.new(host: 'localhost', port: 6379)
+        $redis = Redis.new(url: "redis://redis")
       end
     end
 
